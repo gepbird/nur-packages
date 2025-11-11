9f92e93ce1f5da208e25a2cbfeb0e7deebd5cb75
Gutyina Gergő <gutyina.gergo.2@gmail.com>
2025-11-11 13:28:44 +0100

    lix-curl-patched: fix for CI

--- pkgs/lix-curl-patched/default.nix
+++ pkgs/lix-curl-patched/default.nix
@@ -1,9 +1,9 @@
 # this is a temporary package to build Lix with curl that has a patch for an annoying bug
 # https://github.com/NixOS/nixpkgs/pull/460190
 let
-  pkgs' =
-    import
-      (fetchTarball "https://github.com/NixOS/nixpkgs/archive/804bca8429aaf887cc053266361dfe7498b02b0c.tar.gz")
-      { };
+  pkgs' = import (fetchTarball {
+    url = "https://github.com/NixOS/nixpkgs/archive/804bca8429aaf887cc053266361dfe7498b02b0c.tar.gz";
+    sha256 = "sha256-6SOwcfCaYSBB5IgG/ahS/ho96Prv0LDrBogeiVwvA4s=";
+  }) { };
 in
 pkgs'.lixPackageSets.stable.lix
